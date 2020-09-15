7a42a1095de5ecffb43df0acdedf6bfcde37fa11
maiqamaj <maiqamaj952@gmail.com>
2020-09-16 00:18:36 +0300

    3c03cd4c7d5119b74e04f108680b750fafab03f0

--- cairophp.php
+++ cairophp.php
@@ -3,8 +3,31 @@
 <head>
 <meta charset = "utf-8">
 	<title></title>
+	<style>
+ 		body { background-color: #f5f5f5 }
+
+	 .in {
+                background: #000;
+                padding: 7px;
+                border: 2px outset #fafafa;
+                color: #fafafa
+            }
+            .box1 {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            background-color: #ECE5B6;
+            width: 300px;
+            padding: 30px;
+            border-radius: 10px;
+
+        }
+    
+        </style>
 	</head>
-	<body>
+	<body  >
+	<div class="box1">
 <?php
 if(isset($_POST['Name'])||isset($_POST['email'])||isset($_POST['phone_number']) ){
 $Name=$_POST['Name'];
@@ -16,17 +39,15 @@ $connection = new mysqli("localhost","root","","cairo bank");
 $stmt = $connection->prepare("insert into bank_info(Name,national_number,email,phone_number) values(?,?,?,?);");
 $stmt->bind_param("ssss",$Name,$national_number,$email,$phone_number);
 $stmt->execute();
+
 echo" <center><br><br>Name : $Name<center>"  ; 
-echo" <center>National Number : $national_number<center>"  ; 
 echo" <center>Email : $email<center>"  ; 
+echo" <center>National Number : $national_number<center>"  ; 
 echo" <center>Phone Number : $phone_number <br><center>"  ; 
-
-
 echo "<center><br>Your request has been successfully completed <br><center>";
-echo'THANK YOU '; 
-
-$connection->close();
+echo'<br>THANK YOU!<br><br><br><br>'; 
 ?>
+   <center> <a href="http://localhost/bankbookingsystem.php"> <input type="button" class="in" style="min-width:80px" value="Back To Home Page"></a></center>
  </body>
 
 </HTML>
